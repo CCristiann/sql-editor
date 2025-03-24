f247de62ca5852d8cd00e085e54628b75e74da65
Cristian Cirje <cirjecristian@icloud.com>
2025-03-24 23:34:59 +0100

    Navbar UI fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import Logo from "./Logo";
 import ModeToggle from "./ModeToggle";
+import { buttonVariants } from "./ui/button";
 
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 p-4 bg-white/70 dark:bg-neutral-900/70 backdrop-blur-lg shadow-lg border border-input/50">
       <nav className="max-w-7xl mx-auto w-full">
         <div className="w-full flex justify-between items-center">
-          <Logo />
-          <Link target="_blank" href={"tables"}>
-            View tables
-          </Link>
+          <div className="flex items-center gap-x-10">
+            <Logo />
+            <Link target="_blank" href={"tables"} className={buttonVariants({ variant: "ghost" })}>
+              View tables
+            </Link>
+          </div>
           <ModeToggle />
         </div>
       </nav>
